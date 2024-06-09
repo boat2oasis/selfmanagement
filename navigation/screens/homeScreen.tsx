d1186b464ec8761305c4dc693474f02498829dbf
全干工程师 <5229907+betterzzp@user.noreply.gitee.com>
2024-06-09 23:15:43 +0800

    bugfix

--- navigation/screens/homeScreen.tsx
+++ navigation/screens/homeScreen.tsx
@@ -1,32 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import {Alert, View, ScrollView,Text, StyleSheet,
-  Image, ImageBackground,TextInput,TouchableOpacity} from 'react-native';
+import {View, ScrollView,Text, StyleSheet,Image, ImageBackground} from 'react-native';
 const HomeScreen = () => {
-
-  const [accountList, setAccountList] = useState([]);
-
-
-  let title = "我的账本本"
-
-  interface Accountw {
-    id: number;
-    name: string;
-  }
-  
-  const accountListw: Accountw[] = [
-    { id: 1, name: 'Account1' },
-    { id: 2, name: 'Account2' },
-    { id: 3, name: 'Account3' },
-  ];
-
   interface Account {
     id: number;
     accountName: string;
     accountAmount: string;
     accountIcon: string;
   }
- // let accountList: Account[] = [];
-
+  const [accountList, setAccountList] = useState<Account[]>([])
+  let title="总资产"
   useEffect(() => {
    // debugger
     fetch('http://192.168.11.132:8080/acocunt/list')
@@ -41,18 +23,15 @@ const HomeScreen = () => {
    });
   })
   
- 
   return (
-    <ScrollView style={{backgroundColor:'#FFFFFF'}}>
+   <ScrollView style={{backgroundColor:'#FFFFFF'}}>
    <ImageBackground
       source={require('./assets/background.png')}
       resizeMode="stretch"
       style={styles.image}
-    >
+      >
       <View style={styles.container}>
-        <Text style={styles.text}>{title}</Text>
       </View>
-
       <View style={{alignItems: 'center'}}>
       <View style={styles.totalAssert}>
         <View>
@@ -63,160 +42,54 @@ const HomeScreen = () => {
         </View>
       </View>
       </View>
-
-      <View>
-      <View style={styles.searchAndAddAssert}>
-
-        <View style={styles.searchAssert}> 
-          <Image
-              source={require('./assets/search.png')}
-              style={{width: 16, height: 16,marginLeft:8}}
-            />
-          <TextInput
-            style={{}}
-            placeholder="搜索资产"
-          />
-        </View>
-        <TouchableOpacity style={styles.addAssert}>
-        <Image
-            source={require('./assets/add.png')}
-            style={{width: 16, height: 16,marginLeft:8}}
-          />
-          <Text style={{color:'white',marginLeft:5,marginTop:-3}}>
-            添加资产
-          </Text>
-        </TouchableOpacity>
-      </View>
-      </View>
-      
-      <View style={styles.assertSummery}>
-        <Text style={{color:'black'}}>资金</Text>
-        <View style={styles.assertNumberSummery}>
-          <Text>共</Text>
-          <Text>900</Text>
-        </View>
-      </View>
-
       <View style={{marginTop:12}}>
 
-
-      {accountListw.map((account) => (
-        <Text key={account.id}>{account.name}</Text>
-      ))}
-
-      {accountList.map((item, index) => (
-        
-        <View style={styles.assertsList}>
-        
-        <Image
-              source={require('./assets/zs.png')}
-              style={{width: 28, height: 28}}
+      {accountList.map((item) => (
+          <View style={styles.assertsList}>
+            <View style={[styles.assertsListLeft,styles.assertsListLeftCenter]}>
+            <Image
+                source={{ uri:item.accountIcon}}
+                style={{width: 28, height: 28}}
             />
-          <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>{item.accountName}</Text>
-          <Text style={{marginLeft:120,color:"#FA5A61"}}>{item.accountAmount}</Text>
-          
-        </View>
+            <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>{item.accountName}</Text>
+            </View>
 
+            <View style={styles.assertsListRight}>
+              <Text style={{color:"#FA5A61"}}>{item.accountAmount}</Text>
+            </View>
+          </View>
       ))}
-      
-      
 
-      <View style={styles.assertsList}>
-      <Image
-            source={require('./assets/zgyh.png')}
-            style={{width: 28, height: 28}}
-          />
-        <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>儲蓄卡-中国银行</Text>
-        <Text style={{marginLeft:120,color:"#FA5A61"}}>100.00</Text>
-       
-      </View>
+      {accountList.map((item) => (
+          <View style={styles.assertsList}>
+            <View style={[styles.assertsListLeft,styles.assertsListLeftCenter]}>
+            <Image
+                source={{ uri:item.accountIcon}}
+                style={{width: 28, height: 28}}
+            />
+            <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>{item.accountName}</Text>
+            </View>
 
-      <View style={styles.assertsList}>
-      <Image
-            source={require('./assets/wx.png')}
-            style={{width: 28, height: 28}}
-          />
-        <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>微信支付</Text>
-        <Text style={{marginLeft:168,color:"#FA5A61"}}>100.00</Text>
-       
-      </View>
-
-
-      <View style={styles.assertsList}>
-      <Image
-            source={require('./assets/zfb.png')}
-            style={{width: 28, height: 28}}
-          />
-        <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>支付宝</Text>
-        <Text style={{marginLeft:182,color:"#FA5A61"}}>100.00</Text>
-      </View>
-
-      <View style={styles.assertsList}>
-      <Image
-            source={require('./assets/jj.png')}
-            style={{width: 28, height: 28}}
-          />
-        <Text style={{fontSize:14,color:'#404960',marginLeft:20}}>天天基金</Text>
-        <Text style={{marginLeft:170,color:"#FA5A61"}}>100.00</Text>
-      </View>
-      </View>
+            <View style={styles.assertsListRight}>
+              <Text style={{color:"#FA5A61"}}>{item.accountAmount}</Text>
+            </View>
+          </View>
+      ))}
+    </View>
     </ImageBackground>
     </ScrollView>
   );
 };
 const styles = StyleSheet.create({
-  assertsList:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft:20,
-    marginTop:14
-  },
-  assertNumberSummery:{
-    marginLeft:250,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  assertSummery:{
-    marginTop:20,
-    marginLeft:20,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  searchAssert:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    height:40,
-  
-    marginLeft:20,
-    lineHeight:12,
-    width:190,
-    borderColor:'#000000',
-    backgroundColor:'#F9F9F9',
-    marginTop:16,
-    borderRadius:15,
-    
-  },
-  searchAndAddAssert:{
-    flexDirection: 'row',
-    alignItems: 'center',
-
-  },
-  addAssert: {
-    backgroundColor:'#FAA63D',
-    borderRadius:15,
-    marginLeft:40,
-    width:94,
-    marginTop:16,
-    height:32,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
   image: {
     flex: 1,
   },
+  container: {
+    //marginLeft:20,
+    marginTop:80
+  },
   totalAssert:{
-    marginTop:30,
+    marginTop:60,
     width:320,
     height:82,
     backgroundColor:'white', 
@@ -224,10 +97,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#F9EFEF',
   },
-  container: {
+
+  assertsList:{
+    flexDirection: 'row',
+    alignItems: 'center',
     marginLeft:20,
-    marginTop:45
+    marginRight:20,
+    marginTop:14
   },
+
+  assertsListLeftCenter:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  assertsListLeft: {
+    flex: 3, // 左侧占据剩余空间的1/2
+  },
+  assertsListRight: {
+    flex: 1, // 右侧占据剩余空间的1/2
+  },
+
+
   text: {
     color: 'white',
     fontSize: 18,
